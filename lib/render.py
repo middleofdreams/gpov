4619ab11cec15e189289b0cc980740e4cb3a4185
kuba <middleofdreams@gmail.com>
2009-10-19 14:52:51 +0200

    watek musi sie konczyc...

--- lib/render.py
+++ lib/render.py
@@ -1,22 +1,34 @@
 import threading,events,subprocess,gets,time
 
-def progressbar(progress):
-	while progress !=None:
+class testrunning():
+	def __init__(self):
+		self.running=True
+	def get_running(self):
+		return self.running
+	def set_running(self,running):
+		self.running=running
+	
+	
+def progressbar(progress,var):
+	while var.get_running():
 		time.sleep(0.1)
-		progress.pulse()
+		if progress.child!=None:
+			progress.child.pulse()
+	#var.destroy()
 
-def renderowanie(klasa,options,progress):	
-		proces = subprocess.Popen("povray "+options+" -D", shell=True,stderr=subprocess.PIPE)
-		if gets.getoutput(proces,klasa): 
-			events.pictshow(gets.getpicdir(klasa))
-		
-		progress.destroy()
+def renderowanie(klasa,options,progress,var):	
+	proces = subprocess.Popen("povray "+options+" -D", shell=True,stderr=subprocess.PIPE)
+	if gets.getoutput(proces,klasa): 
+		events.pictshow(gets.getpicdir(klasa))
+	var.set_running(False)
+	progress.destroy()
 
 		
 
 def threadinit(klasa,options,progress):
-	threading.Thread(target=progressbar,args=(progress)).start()
-	threading.Thread(target=renderowanie,args=(klasa,options,progress)).start()
+	var=testrunning()
+	threading.Thread(target=progressbar,args=(progress,var)).start()
+	threading.Thread(target=renderowanie,args=(klasa,options,progress,var)).start()
 
 
 
